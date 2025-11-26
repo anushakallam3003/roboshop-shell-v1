e7f5aba0f233b90efec6d7d99224db8d3ac81870
AnushaKallam3003 <kallamanusha67@gmail.com>
2025-11-26 14:23:21 +0530

    shipping script

--- rabbitmq.sh
+++
@@ -1,8 +0,0 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-
